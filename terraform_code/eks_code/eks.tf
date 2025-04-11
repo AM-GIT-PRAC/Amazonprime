42655aed6d957cbc5c6cb92a3c88a7b5af1d36c2
AM-GIT-PRAC <makthala.a@outlook.com>
2025-04-10 17:41:36 -0700

    Update eks.tf

--- terraform_code/eks_code/eks.tf
+++ terraform_code/eks_code/eks.tf
@@ -22,9 +22,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     panda-node = {
-      min_size     = 2
-      max_size     = 4
-      desired_size = 2
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
 
       instance_types = ["t2.medium"]
       capacity_type  = "SPOT"
